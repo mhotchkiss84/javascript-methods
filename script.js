13cf4cc6ddb86503121e82fa78b0aec0452bcad7
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-05-11 15:01:03 -0400

    Added JSON file and started car dealership exercises

--- script.js
+++ script.js
@@ -129,11 +129,10 @@ const outputContainer = document.querySelector(`#output`);
 const bigSpenders = businesses.filter(business => {
 	business.orders.forEach(element => {
 		if(element > 9000){
-			// console.log(business.companyName)
+		// console.log(business.companyName)
 		}
 	});
 })
-
 // Filter method
 // Filter for businesses in New York
 const newYorkBusinessArray = businesses.filter((singleBusiness) => singleBusiness.addressStateCode === 'NY');
@@ -216,7 +215,7 @@ planets.forEach(element => {
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 const upperCasePlanets = planets.map(x => x.charAt(0).toUpperCase() + x.slice(1))
-console.log(upperCasePlanets)
+// console.log(upperCasePlanets)
 
 /*
     Use the filter method to create a new array that
@@ -225,3 +224,298 @@ console.log(upperCasePlanets)
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+
+const planetsWithLetterE = planets.filter((planetsEithE) => planetsEithE.includes(`e`))
+// console.log(planetsWithLetterE)
+
+// const newYorkBusinessArray = businesses.filter((singleBusiness) => singleBusiness.addressStateCode === 'NY');
+
+// Practice: Spam, Spam, Spam, Spam
+let customers = [
+	{
+	  "location": {
+		"street": "Old York Road",
+		"state": "Delaware",
+		"country": "Qatar",
+		"city": "Irma"
+	  },
+	  "last_name": "Herzog",
+	  "job": {
+		"position": "Senior Intranet Assistant",
+		"phone_number": "1-035-577-1574",
+		"department": "sales",
+		"company": "Ganjazamlux"
+	  },
+	  "first_name": "Johnson",
+	  "contacts": {
+		"mobile": "102.596.5226",
+		"email": [
+		  "rerum@outlook.com",
+		  "possimus_abraham@hotmail.com",
+		  "yoshiko@gmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Maple Street",
+		"state": "Wyoming",
+		"country": "Uzbekistan",
+		"city": "Ortonville"
+	  },
+	  "last_name": "Stehr",
+	  "job": {
+		"position": "Dynamic Quality Administrator",
+		"phone_number": "555.173.9685",
+		"department": "customer service support",
+		"company": "Hating"
+	  },
+	  "first_name": "Rogelio",
+	  "contacts": {
+		"mobile": "1-746-406-3132",
+		"email": [
+		  "saw_savannah@yahoo.com",
+		  "this46@outlook.com",
+		  "quidem_shayna@hotmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Cleveland Avenue",
+		"state": "South Carolina",
+		"country": "Lebanon",
+		"city": "New Salem"
+	  },
+	  "last_name": "McDermott",
+	  "job": {
+		"position": "Human Research Architect",
+		"phone_number": "1-120-949-9685",
+		"department": "accounting",
+		"company": "Keyzazim"
+	  },
+	  "first_name": "Herbert",
+	  "contacts": {
+		"mobile": "705.404.8808",
+		"email": [
+		  "him_joshuah@yahoo.com",
+		  "neha@outlook.com",
+		  "tail@gmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Route 30",
+		"state": "Pennsylvania",
+		"country": "Nauru",
+		"city": "Brocton"
+	  },
+	  "last_name": "Buckridge",
+	  "job": {
+		"position": "National Usability Technician",
+		"phone_number": "215.951.1943",
+		"department": "research and development",
+		"company": "Dongsolofind"
+	  },
+	  "first_name": "Keagan",
+	  "contacts": {
+		"mobile": "665.835.9595",
+		"email": [
+		  "trade_hunter@yahoo.com",
+		  "ut@hotmail.com",
+		  "throw@gmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Arch Street",
+		"state": "Illinois",
+		"country": "Guam",
+		"city": "Stroud"
+	  },
+	  "last_name": "Barton",
+	  "job": {
+		"position": "Dynamic Directives Analyst",
+		"phone_number": "1-069-596-0831 x15766",
+		"department": "operations",
+		"company": "Zoomruntam"
+	  },
+	  "first_name": "Kenton",
+	  "contacts": {
+		"mobile": "(972) 219-3017",
+		"email": [
+		  "alberto@outlook.com",
+		  "june@gmail.com",
+		  "rafaela@hotmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Race Street",
+		"state": "Nebraska",
+		"country": "Republic of Saudi Arabia",
+		"city": "Guelph"
+	  },
+	  "last_name": "Mitchell",
+	  "job": {
+		"position": "Principal Implementation Specialist",
+		"phone_number": "713-818-2034 x23626",
+		"department": "IT support",
+		"company": "Hot-tom"
+	  },
+	  "first_name": "Hattie",
+	  "contacts": {
+		"mobile": "(144) 533-6049",
+		"email": [
+		  "river32@yahoo.com",
+		  "class65@gmail.com",
+		  "scale_oda@outlook.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Prospect Avenue",
+		"state": "Massachusetts",
+		"country": "Fiji",
+		"city": "Hachita"
+	  },
+	  "last_name": "Rohan",
+	  "job": {
+		"position": "Customer Identity Producer",
+		"phone_number": "110-592-6773 x3160",
+		"department": "production",
+		"company": "Zapware"
+	  },
+	  "first_name": "Clotilde",
+	  "contacts": {
+		"mobile": "005.981.1039",
+		"email": [
+		  "winter@outlook.com",
+		  "kyla@gmail.com",
+		  "boy@yahoo.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Lexington Court",
+		"state": "Georgia",
+		"country": "Tokelau",
+		"city": "Blackwater"
+	  },
+	  "last_name": "Kuphal",
+	  "job": {
+		"position": "Direct Division Executive",
+		"phone_number": "(763) 900-2576 x943",
+		"department": "operations",
+		"company": "Inchdonin"
+	  },
+	  "first_name": "Marcelle",
+	  "contacts": {
+		"mobile": "1-511-518-7079",
+		"email": [
+		  "try@outlook.com",
+		  "part73@yahoo.com",
+		  "mother5@hotmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Cobblestone Court",
+		"state": "Minnesota",
+		"country": "Saint Helena, Ascension and Tristan da Cunha",
+		"city": "North Stonington"
+	  },
+	  "last_name": "Thompson",
+	  "job": {
+		"position": "Product Response Facilitator",
+		"phone_number": "(948) 430-6348 x4290",
+		"department": "communication",
+		"company": "Goldcare"
+	  },
+	  "first_name": "Briana",
+	  "contacts": {
+		"mobile": "714-922-9557",
+		"email": [
+		  "randi@outlook.com",
+		  "christian@yahoo.com",
+		  "tempore_gilda@hotmail.com"
+		]
+	  }
+	},
+	{
+	  "location": {
+		"street": "Mill Street",
+		"state": "Kentucky",
+		"country": "Ireland",
+		"city": "Crystal"
+	  },
+	  "last_name": "Weimann",
+	  "job": {
+		"position": "Future Web Representative",
+		"phone_number": "693-204-7849 x4251",
+		"department": "legal",
+		"company": "K-touch"
+	  },
+	  "first_name": "Brittany",
+	  "contacts": {
+		"mobile": "1-327-084-4643",
+		"email": [
+		  "she@hotmail.com",
+		  "quam3@gmail.com",
+		  "recusandae23@outlook.com"
+		]
+	  }
+	}
+  ];
+// From that array, extract just the customers' contact email addresses and store them in a new array. (Hint: look up the .flat() array method!)
+const customerEmail = customers.map(singleCustomer => {
+	return{
+		customerEmail: singleCustomer.contacts.email.flat()
+	}
+})
+console.log(customerEmail)
+
+// Challenge: Chaining Methods
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+// Remove any integers greater than 19.
+// Multiply each remaining number by 1.5 and then subtract 1.
+// Then output (either in the DOM or the console) the sum of all the resulting numbers.
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+
+const integersDecending = integers.sort((a, b) => a > b ? 1 : -1).filter(number => {
+	return number < 19;
+}).reduce((numbersTotal, currentValue) => numbersTotal += currentValue * 1.5 - 1) 
+console.log(integersDecending)
+
+// Map then reduce
+
+
+// Advanced Challenge: Car Dealership
+let dealershipProfit = 0
+fetch(`http://localhost:8088/carSales`)
+.then((carSales) => carSales.json())
+.then((parsedSales) => {
+	parsedSales.forEach(element => {
+		dealershipProfit += element.gross_profit
+	});
+	console.log(dealershipProfit)
+})
+
+
+// Total profit for 2017
+
+// In which month did they sell the most cars?
+let months = []
+
+// Which salesperson sold the most cars?
+// Which salesperson made the most profit?
+// Which model was the most popular?
+// Which bank provided the most loans to our customers?
+// In which month did zero cars get sold?
